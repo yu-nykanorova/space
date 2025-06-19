69c9ca056e98f85b4d2eb86dd0ed21921a4f3548
Yuliia Nykanorova <juliyasos@ukr.net>
2025-06-19 20:06:21 +0300

    added structure and styles to the component NotFound

--- src/components/notFound/NotFound.jsx
+++ src/components/notFound/NotFound.jsx
@@ -1,7 +1,12 @@
+import { Banner } from '../banner/Banner';
 import './NotFound.scss';
+import '../banner/Banner.scss';
 
 export const NotFound = () => {
   return (
-    <div>Sorry, this page was not found</div>
+    <>
+      <Banner className="banner-planets"/>
+      <div className="not-found">Sorry, this page was not found</div>
+    </>
   )
 };
